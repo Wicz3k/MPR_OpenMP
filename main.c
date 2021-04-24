53d1905a5eb44a3ffe40b839b747e2e3ea655e80
wiczek2 <wiczek2@poczta.onet.pl>
2021-04-24 18:46:24 +0200

    Randomize seeds

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 int main(int argc, char *argv[])
 {        
+    srand(time(NULL));
     unsigned long long table_size = 0;
     if(argc < 2){
         printf("Parameters: table_size\n");
@@ -17,10 +18,26 @@ int main(int argc, char *argv[])
         printf("Could not allocate table_size: %lld\n", table_size);
         return -1;
     }
+    int maxNThreads = 1;
+    #pragma omp parallel
+    {
+        if(omp_get_thread_num() == 0)
+        {
+            maxNThreads = omp_get_num_threads();
+        }
+    }
+
+    unsigned int* seeds;
+    seeds = malloc(maxNThreads * sizeof(unsigned int));
+    int tn;
+    for (tn = 0; tn<maxNThreads; tn++){
+        seeds[tn] = rand();
+    } 
+
     #pragma omp parallel
     {
         int i;
-        unsigned int myseed = omp_get_thread_num();
+        unsigned int myseed = seeds[omp_get_thread_num()];
         #pragma omp for
         for(i=0 ; i < table_size ; i++){
             v[i] = rand_r(&myseed);
@@ -30,6 +47,7 @@ int main(int argc, char *argv[])
         
     }
 
+    free(seeds);
     free(v);
     return 0;
 }
